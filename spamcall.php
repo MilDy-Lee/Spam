806f6ca22c9eb5ee69623f0cfd89c0687a1b1a33
MilDy-Lee <67098490+MilDy-Lee@users.noreply.github.com>
2020-06-18 19:14:23 +0700

    Update spamcall.php

--- spamcall.php
+++ spamcall.php
@@ -1,5 +1,5 @@
 <?php
-// Limit 3x Telpon Setiap Satu Nomor
+// Limit Telponnya bro jangan keseringan
 function send($phone){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -14,10 +14,10 @@ function send($phone){
 echo "
 ############################################################
     Spammer Call From TOKOPEDIA
-        Author By   : AyipBontos
-        Support     : Kunjungi Website Kami
-        Website     : https://termux.id
-        Youtube     : Omalip TV
+        Author By   : MilDyLee
+        Support IG  : hilmifav_
+        Whatsaol    : 083846132765
+        Youtube     : Fepscoverenigen78
      Penggunaannya  : Bisa Menggunakan [08/62]
 ###########################################################\n";
 echo "Nomor\nInput : ";
